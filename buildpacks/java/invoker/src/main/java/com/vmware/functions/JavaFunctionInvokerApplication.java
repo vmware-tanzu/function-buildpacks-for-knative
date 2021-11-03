c8f6aae7f3f65eab0c1de4c5c7b3357063e1b522
Bryan Tong <tongb@vmware.com>
2021-11-03 15:57:54 -0700

    Add Java invoker

---
+++ buildpacks/java/invoker/src/main/java/com/vmware/functions/JavaFunctionInvokerApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.functions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class JavaFunctionInvokerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFunctionInvokerApplication.class, args);
+	}
+}
---
+++ buildpacks/java/invoker/src/test/java/com/vmware/functions/JavaFunctionInvokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.functions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFunctionInvokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
