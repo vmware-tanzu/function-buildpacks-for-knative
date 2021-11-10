b7a3c74be8f1b87118a682b2140c88f7662cac6d
Andrew Su <suan@vmware.com>
2021-11-10 11:53:41 -0500

    Add http adding function with "authentication"

---
+++ samples/python/http/adder/handler.py
@@ -0,0 +1,23 @@
+from typing import Any
+import flask
+
+# This is a toy sample and should not be used as an example of how to authenticate a user.
+# Credentials should never be stored like this and errors returned should not be so specific.
+
+creds = {"admin": "supersecure", "asu": "mypassword", "someone": "123qwe"}
+
+def add(req: Any):
+    username = req.form.get('username')
+    if username not in creds:
+        return flask.Response("Unauthroized: User does not exist", 401)
+    
+    password = req.form.get("password")
+    if password != creds[username]:
+        return flask.Response("Unauthorized: Wrong password", 401) # Return your own flask response to set a return code!
+
+    first = req.form.get("first", default=0)
+    second = req.form.get("second", default=0)
+
+    sum = int(first) + int(second)
+    return f"Hello, {username}! The answer to {first} + {second} is {sum}" # Without returning a flask response, it'll automatically be HTTPOK
+    
