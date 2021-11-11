10632e89db40c28c36aa30142d11e478f162617f
Bryan Tong <tongb@vmware.com>
2021-11-10 18:42:56 -0800

    wip: add cloudevent smoke testing

--- tests/go.mod
+++ tests/go.mod
@@ -9,18 +9,26 @@ require (
 
 require (
 	github.com/Microsoft/go-winio v0.4.17 // indirect
+	github.com/cloudevents/sdk-go/v2 v2.6.1 // indirect
 	github.com/containerd/containerd v1.5.7 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
--- tests/smoke_test.go
+++ tests/smoke_test.go
@@ -2,8 +2,12 @@ package tests
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"io/ioutil"
+	"log"
+
 	"net/http"
 	"strings"
 	"testing"
@@ -89,6 +93,80 @@ func TestHelloWorldHTTP(t *testing.T) {
 			if bs != c.expectedResponse {
 				t.Errorf("Expected response '%s' but received '%s'.", c.expectedResponse, bs)
 			}
+		})
+	}
+}
+
+func TestHelloWorldCloudEvents(t *testing.T) {
+	baseImage := "kn-fn-test/helloworld"
+	cases := []struct {
+		name string
+		tag  string
+
+		methodType       string
+		contentType      string
+		path             string
+		expectedResponse string
+	}{
+		{
+			name: "Java",
+			tag:  "java",
+
+			methodType:       "example.type",
+			path:             "/hello",
+			expectedResponse: "Hello World!",
+		},
+		{
+			name: "Python",
+			tag:  "python",
+
+			methodType:       "example.type",
+			path:             "/",
+			expectedResponse: "Hello World!",
+		},
+	}
+
+	// Create a CloudEvents client
+	client, err := cloudevents.NewClientHTTP()
+	if err != nil {
+		log.Fatalf("failed to create client, %v", err)
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			image := fmt.Sprintf("%s:%s", baseImage, c.tag)
+			cleanup, err := runTestContainer(image)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			defer cleanup()
+
+			url := fmt.Sprintf("http://127.0.0.1:8080/%s", strings.TrimLeft(c.path, "/"))
+
+			// Send a CloudEvent
+			event := cloudevents.NewEvent()
+			event.SetSource("example/uri")
+			event.SetType("example.type")
+			event.SetData(cloudevents.ApplicationJSON, "Hello World!")
+
+			// Set a target.
+			ctx := cloudevents.ContextWithTarget(context.Background(), url)
+
+			// Send that Event.
+			if result := client.Send(ctx, event); cloudevents.IsUndelivered(result) {
+				log.Fatalf("failed to send, %v", result)
+			}
+
+			// Receive a CloudEvent
+			log.Fatal(client.StartReceiver(context.Background(), func(e cloudevents.Event) {
+				bs := string(e.Data()[:])
+				if bs != c.expectedResponse {
+					t.Errorf("Expected response '%s' but received '%s'.", c.expectedResponse, bs)
+				}
+			}))
+
 		})
 	}
 }
