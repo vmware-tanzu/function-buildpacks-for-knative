6f563af893a84f09cf9d5c39824288047f73267e
Bryan Tong <tongb@vmware.com>
2021-11-29 17:08:47 -0800

    add experiment
    Signed-off-by: Bryan Tong <tongb@vmware.com>

--- tests/cases/echo-ce/python/handler.py
+++ tests/cases/echo-ce/python/handler.py
@@ -1,8 +1,13 @@
 import logging
 from typing import Any
 
-def handler(data: Any, attributes: Any):
-    payload = data
-    if 'my-header' not in attributes or attributes['my-header'] != "test":
-        return attributes, "Incorrect 'my-header' value in request"
-    return attributes, payload
+def handler(data: Any, attributes: dict):
+    # Your function implementation goes here
+    print("I'M HERE IM THE HANDLER")
+    return attributes, "Hello World!"
+
+# def handler(attributes: Any, data: Any):
+#     payload = data
+#     if 'my-header' not in attributes or attributes['my-header'] != "test":
+#         return attributes, "Incorrect 'my-header' value in request"
+#     return attributes, payload
