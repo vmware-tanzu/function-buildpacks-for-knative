f198d8e15c9f150b4f91c54f9620196c85880829
Bryan Tong <tongb@vmware.com>
2021-11-29 18:27:37 -0800

    Python pass, Java fails
    Signed-off-by: Bryan Tong <tongb@vmware.com>

--- tests/smoke_test.go
+++ tests/smoke_test.go
@@ -1,103 +1,104 @@
 package tests
 
 import (
-	"bytes"
+	// "bytes"
 	"context"
 	"fmt"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
-	"io/ioutil"
+	// "io/ioutil"
 
-	"net/http"
+	// "net/http"
+	"github.com/google/uuid"
 	"strings"
 	"testing"
 )
 
-func TestHelloWorldHTTP(t *testing.T) {
-	baseImage := "kn-fn-test/helloworld"
-	cases := []struct {
-		name string
-		tag  string
+// func TestHelloWorldHTTP(t *testing.T) {
+// 	baseImage := "kn-fn-test/helloworld"
+// 	cases := []struct {
+// 		name string
+// 		tag  string
 
-		methodType       string
-		contentType      string
-		path             string
-		expectedResponse string
-	}{
-		{
-			name: "Java",
-			tag:  "java",
+// 		methodType       string
+// 		contentType      string
+// 		path             string
+// 		expectedResponse string
+// 	}{
+// 		{
+// 			name: "Java",
+// 			tag:  "java",
 
-			methodType:       http.MethodPost,
-			path:             "/hello",
-			expectedResponse: "Hello World!",
-		},
-		{
-			name: "Python GET",
-			tag:  "python",
+// 			methodType:       http.MethodPost,
+// 			path:             "/hello",
+// 			expectedResponse: "Hello World!",
+// 		},
+// 		{
+// 			name: "Python GET",
+// 			tag:  "python",
 
-			methodType:       http.MethodGet,
-			path:             "/",
-			expectedResponse: "Hello World!",
-		},
-		{
-			name: "Python POST",
-			tag:  "python",
+// 			methodType:       http.MethodGet,
+// 			path:             "/",
+// 			expectedResponse: "Hello World!",
+// 		},
+// 		{
+// 			name: "Python POST",
+// 			tag:  "python",
 
-			methodType:       http.MethodPost,
-			path:             "/",
-			expectedResponse: "Hello World!",
-		},
-	}
+// 			methodType:       http.MethodPost,
+// 			path:             "/",
+// 			expectedResponse: "Hello World!",
+// 		},
+// 	}
 
-	for _, c := range cases {
-		c := c
-		t.Run(c.name, func(t *testing.T) {
-			image := fmt.Sprintf("%s:%s", baseImage, c.tag)
-			cleanup, err := runTestContainer(image)
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			defer cleanup()
+// 	for _, c := range cases {
+// 		c := c
+// 		t.Run(c.name, func(t *testing.T) {
+// 			image := fmt.Sprintf("%s:%s", baseImage, c.tag)
+// 			cleanup, err := runTestContainer(image)
+// 			if err != nil {
+// 				t.Error(err)
+// 				return
+// 			}
+// 			defer cleanup()
 
-			url := fmt.Sprintf("http://127.0.0.1:8080/%s", strings.TrimLeft(c.path, "/"))
+// 			url := fmt.Sprintf("http://127.0.0.1:8080/%s", strings.TrimLeft(c.path, "/"))
 
-			var resp *http.Response
-			switch c.methodType {
-			case http.MethodGet:
-				resp, err = http.Get(url)
-				if err != nil {
-					t.Error(err)
-					return
-				}
-			case http.MethodPost:
-				ct := c.contentType
-				if ct == "" {
-					ct = "application/json"
-				}
-				resp, err = http.Post(url, ct, bytes.NewBufferString(""))
-				if err != nil {
-					t.Error(err)
-					return
-				}
-			}
-			defer resp.Body.Close()
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				t.Error(err)
-				return
-			}
+// 			var resp *http.Response
+// 			switch c.methodType {
+// 			case http.MethodGet:
+// 				resp, err = http.Get(url)
+// 				if err != nil {
+// 					t.Error(err)
+// 					return
+// 				}
+// 			case http.MethodPost:
+// 				ct := c.contentType
+// 				if ct == "" {
+// 					ct = "application/json"
+// 				}
+// 				resp, err = http.Post(url, ct, bytes.NewBufferString(""))
+// 				if err != nil {
+// 					t.Error(err)
+// 					return
+// 				}
+// 			}
+// 			defer resp.Body.Close()
+// 			body, err := ioutil.ReadAll(resp.Body)
+// 			if err != nil {
+// 				t.Error(err)
+// 				return
+// 			}
 
-			bs := string(body)
-			if bs != c.expectedResponse {
-				t.Errorf("Expected response '%s' but received '%s'.", c.expectedResponse, bs)
-			}
-		})
-	}
-}
+// 			bs := string(body)
+// 			if bs != c.expectedResponse {
+// 				t.Errorf("Expected response '%s' but received '%s'.", c.expectedResponse, bs)
+// 			}
+// 		})
+// 	}
+// }
 
 func TestHelloWorldCloudEvents(t *testing.T) {
-	baseImage := "kn-fn-test/helloworld"
+	baseImage := "kn-fn-test/echo-ce"
 	cases := []struct {
 		name string
 		tag  string
@@ -111,7 +112,7 @@ func TestHelloWorldCloudEvents(t *testing.T) {
 			name: "Java",
 			tag:  "java",
 
-			path:             "/hello",
+			path:             "/echo",
 			expectedResponse: "Hello World!",
 		},
 		{
@@ -143,11 +144,20 @@ func TestHelloWorldCloudEvents(t *testing.T) {
 
 			event := cloudevents.NewEvent()
 			event.SetSource("url")
+			event.SetID(uuid.New().String())
 			event.SetType("example.type")
 			event.SetData(cloudevents.TextPlain, "Hello World!")
 
+			fmt.Println(event)
+
 			ctx := cloudevents.ContextWithTarget(context.Background(), url)
 			reqEvent, result := client.Request(ctx, event)
+
+			fmt.Println("Result")
+			fmt.Println(result)
+			fmt.Println("ReqEvent")
+			fmt.Println(reqEvent)
+
 			if cloudevents.IsUndelivered(result) {
 				t.Error(err)
 			}
