b9f953a1cb1326ec0ca09ea8e8a68bd824269a38
Bryan Tong <tongb@vmware.com>
2021-12-06 12:41:03 -0800

    Add passing CE
    Signed-off-by: Bryan Tong <tongb@vmware.com>

--- templates/java/cloudevents/src/main/java/com/vmware/functions/Handler.java
+++ templates/java/cloudevents/src/main/java/com/vmware/functions/Handler.java
@@ -1,14 +1,13 @@
 package com.vmware.functions;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.Message;
-
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
 
 import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
@@ -19,15 +18,26 @@ public class Handler {
 		SpringApplication.run(Handler.class, args);
 	}
 
-	@Bean
-	public Function<CloudEvent, CloudEvent> hello() {
-        CloudEvent outgoingCloudEvent = CloudEventBuilder.v1()
-               .withId("my-id")
-               .withSource(URI.create("/my-test"))
-               .withType("function-reply")
-               .withDataContentType("application/json")
-               .withData("{ \"msg\" : \"hello\" }".getBytes(StandardCharsets.UTF_8))
-               .build();
-        return outgoingCloudEvent;
+	// @Bean
+	// public Function<CloudEvent, CloudEvent> hello() {
+    //     CloudEvent outgoingCloudEvent = CloudEventBuilder.v1()
+    //            .withId("my-id")
+    //            .withSource(URI.create("/my-test"))
+    //            .withType("function-reply")
+    //            .withDataContentType("application/json")
+    //            .withData("{ \"msg\" : \"hello\" }".getBytes(StandardCharsets.UTF_8))
+    //            .build();
+    //     return outgoingCloudEvent;
+    // }
+
+    @Bean
+    public Function<CloudEvent, CloudEvent> hello() {
+    return event -> CloudEventBuilder.from(event)
+        .withId(UUID.randomUUID().toString())
+        .withSource(URI.create("/hello-world"))
+        .withType("function-reply")
+        .withData("{ \"msg\" : \"Hello World!\" }".getBytes(StandardCharsets.UTF_8))
+        .build();
     }
+
 }
