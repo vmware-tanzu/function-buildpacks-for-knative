5d4f9f5b2ea9afd2448fa943262a03dc90148c4c
Andrew Su <suan@vmware.com>
2021-12-10 11:37:49 -0500

    Fix java http sample
    Signed-off-by: Andrew Su <suan@vmware.com>

--- samples/java/http/java-http-sample/src/main/java/functions/HTTPFunc.java
+++ samples/java/http/java-http-sample/src/main/java/functions/HTTPFunc.java
@@ -1,6 +1,9 @@
 package functions;
 
 import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.function.Function;
 
 public class HTTPFunc implements Function<String, String> {
@@ -11,10 +14,10 @@ public class HTTPFunc implements Function<String, String> {
                 .uri(URI.create("http://example.com"))
                 .build();
 
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-
-        System.out.println("HTTP GET request sent");
-        return response.body();
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch website");
+        }
     }
 }
