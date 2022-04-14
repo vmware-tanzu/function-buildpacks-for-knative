fcd166f7af93407876c78a8a9d450bd338ae2285
Joe Eltgroth <jeltgroth@vmware.com>
2022-04-14 13:11:10 -0500

    Replace the test cases with symlinks to the templates
    Signed-off-by: Joe Eltgroth <jeltgroth@vmware.com>

--- tests/cases/template-ce/python-cloudevents/func.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2021-2022 VMware, Inc.
-# SPDX-License-Identifier: BSD-2-Clause
-
-from typing import Any
-
-def main(data: Any, attributes: dict):
-    # Your function implementation goes here
-    return attributes, data
