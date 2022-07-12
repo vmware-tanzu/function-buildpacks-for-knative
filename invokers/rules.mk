c376f64e8f1a38c1e1f74765b28af59c47275c2e
Andrew Su <suan@vmware.com>
2022-07-12 15:16:48 -0400

    chore: update invoker build targets
    Signed-off-by: Andrew Su <suan@vmware.com>

--- invokers/rules.mk
+++ invokers/rules.mk
@@ -1,3 +1,3 @@
-invoker.bucket = gs://cnr-functions-release/kn-fn
-invoker.build_dir = $(build_dir)/invokers
-invoker.out_dir = $(out_dir)/invokers
+invokers.bucket = gs://cnr-functions-release/kn-fn
+invokers.build_dir = $(build_dir)/invokers
+invokers.out_dir = $(out_dir)/invokers
