9ae2e8b9d0a87fefbf1957341e78299c16b9ad13
Andrew Su <suan@vmware.com>
2022-08-15 12:32:44 -0400

    chore(tests): Add tests for ensuring the restore functionality works
    Ensure the caching works and updates accordingly
    
    Signed-off-by: Andrew Su <suan@vmware.com>

--- tests/go.mod
+++ tests/go.mod
@@ -7,32 +7,51 @@ require (
 	github.com/docker/docker v20.10.17+incompatible
 	github.com/docker/go-connections v0.4.0
 	github.com/google/uuid v1.3.0
+	github.com/onsi/gomega v1.20.0
+	github.com/paketo-buildpacks/occam v0.12.0
+	github.com/paketo-buildpacks/packit/v2 v2.3.1
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/ForestEckhardt/freezer v0.0.11 // indirect
 	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/Microsoft/hcsshim v0.9.3 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/containerd/cgroups v1.0.3 // indirect
 	github.com/containerd/containerd v1.6.6 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/moby/sys/mount v0.2.0 // indirect
+	github.com/moby/sys/mountinfo v0.5.0 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
+	github.com/opencontainers/image-spec v1.0.3-0.20220114050600-8b9d41f48198 // indirect
+	github.com/opencontainers/runc v1.1.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/testcontainers/testcontainers-go v0.13.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/goleak v1.1.12 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.18.1 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/net v0.0.0-20220614195744-fb05da6f9022 // indirect
 	golang.org/x/sys v0.0.0-20220614162138-6c1b26c55098 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220614165028-45ed7f3ff16e // indirect
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ tests/integration/restore_test.go
@@ -0,0 +1,125 @@
+// Copyright 2021-2022 VMware, Inc.
+// SPDX-License-Identifier: BSD-2-Clause
+
+package integration
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+	"github.com/paketo-buildpacks/occam"
+	. "github.com/paketo-buildpacks/occam/matchers"
+)
+
+var restoreTestData = filepath.Join("..", "testdata", "restore")
+
+func TestRestorePython(t *testing.T) {
+	t.Parallel()
+	Expect := NewWithT(t).Expect
+	Eventually := NewWithT(t).Eventually
+
+	name, err := occam.RandomName()
+	Expect(err).ToNot((HaveOccurred()))
+
+	source1, err := occam.Source(filepath.Join(restoreTestData, "python", "step1"))
+	Expect(err).NotTo(HaveOccurred())
+	defer os.RemoveAll(source1)
+
+	source2, err := occam.Source(filepath.Join(restoreTestData, "python", "step2"))
+	Expect(err).NotTo(HaveOccurred())
+	defer os.RemoveAll(source2)
+
+	docker := NewDocker()
+
+	// Do the first build
+	{
+		image, logs, err := PackBuild(name, source1)
+		Expect(err).NotTo(HaveOccurred(), logs.String())
+		defer docker.Image.Remove.Execute(image.ID)
+	}
+
+	// Do the second build
+	image, logs, err := PackBuild(name, source2)
+	Expect(err).NotTo(HaveOccurred(), logs.String())
+	defer docker.Image.Remove.Execute(image.ID)
+
+	port, err := GetFreePort()
+	Expect(err).ToNot(HaveOccurred())
+
+	container, err := docker.Container.Run.
+		WithPublish(port + ":8080").
+		Execute(image.ID)
+	Expect(err).NotTo(HaveOccurred(), logs.String())
+	defer docker.Container.Remove.Execute(container.ID)
+	defer docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))
+
+	Eventually(container, 2*time.Minute, 10*time.Second).Should(BeAvailable())
+
+	response, err := http.Post(fmt.Sprintf("http://localhost:%s", container.HostPort("8080")), "application/json", bytes.NewBufferString(""))
+	Expect(err).NotTo(HaveOccurred())
+	defer response.Body.Close()
+	Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+	content, err := io.ReadAll(response.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(string(content)).To(Equal("Bye world!"))
+}
+
+func TestRestoreJava(t *testing.T) {
+	t.Parallel()
+	Expect := NewWithT(t).Expect
+	Eventually := NewWithT(t).Eventually
+
+	name, err := occam.RandomName()
+	Expect(err).ToNot((HaveOccurred()))
+
+	source1, err := occam.Source(filepath.Join(restoreTestData, "java", "step1"))
+	Expect(err).NotTo(HaveOccurred())
+	defer os.RemoveAll(source1)
+
+	source2, err := occam.Source(filepath.Join(restoreTestData, "java", "step2"))
+	Expect(err).NotTo(HaveOccurred())
+	defer os.RemoveAll(source2)
+
+	docker := NewDocker()
+
+	// Do the first build
+	{
+		image, logs, err := PackBuild(name, source1)
+		Expect(err).NotTo(HaveOccurred(), logs.String())
+		defer docker.Image.Remove.Execute(image.ID)
+	}
+
+	// Do the second build
+	image, logs, err := PackBuild(name, source2)
+	Expect(err).NotTo(HaveOccurred(), logs.String())
+	defer docker.Image.Remove.Execute(image.ID)
+
+	port, err := GetFreePort()
+	Expect(err).ToNot(HaveOccurred())
+
+	container, err := docker.Container.Run.
+		WithPublish(port + ":8080").
+		Execute(image.ID)
+	Expect(err).NotTo(HaveOccurred(), logs.String())
+	defer docker.Container.Remove.Execute(container.ID)
+	defer docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))
+
+	Eventually(container, 2*time.Minute, 10*time.Second).Should(BeAvailable())
+
+	response, err := http.Post(fmt.Sprintf("http://localhost:%s", container.HostPort("8080")), "application/json", bytes.NewBufferString(""))
+	Expect(err).NotTo(HaveOccurred())
+	defer response.Body.Close()
+	Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+	content, err := io.ReadAll(response.Body)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(string(content)).To(Equal("Bye world!"))
+}
