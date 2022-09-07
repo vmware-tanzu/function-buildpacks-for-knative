58fadc1e7ca2daf0db31c827d34d2b32f01ff79b
Andrew Meyer <meyera@vmware.com>
2022-09-06 17:42:19 -0700

    Make contributing a bit easier/clearer
    Signed-off-by: Andrew Meyer <meyera@vmware.com>

---
+++ local.example.mk
@@ -0,0 +1,6 @@
+# NOTE: Copy this file and rename to `local.mk`, then make necessary adjustments.
+
+# IMPORTANT: Run `make clean` after adjustments to registry location.
+registry.location := local
+# registry.location := other  # alternative - requires `REGISTRY` env var to be set.
+registry.local := example.com/registry
