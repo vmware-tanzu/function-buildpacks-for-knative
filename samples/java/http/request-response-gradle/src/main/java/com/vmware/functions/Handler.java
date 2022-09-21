1816ee9c6c298e9692757e02a50a7b5133700966
Joe Eltgroth <jeltgroth@vmware.com>
2022-09-21 14:29:53 -0500

    Roll back the earlier java http function changes and replace with a build.gradle fix
    Signed-off-by: Joe Eltgroth <jeltgroth@vmware.com>

---
+++ samples/java/http/request-response-gradle/src/main/java/com/vmware/functions/Handler.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021-2022 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+package com.vmware.functions;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+@SpringBootApplication
+public class Handler {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Handler.class, args);
+	}
+
+	@Bean
+	public Function<String, String> hello() {
+		return in -> {
+			HttpClient client = HttpClient.newHttpClient();
+			// Replace with your request
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create("http://example.com"))
+					.build();
+	
+			try {
+				return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+			} catch (Exception e) {
+				throw new RuntimeException("Failed to fetch website");
+			}
+		};
+	}
+}
